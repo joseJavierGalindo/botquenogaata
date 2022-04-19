07bc5fbcf483d09292dee016df4fc3a2106d79b0
joseJavierGalindo <100740011+joseJavierGalindo@users.noreply.github.com>
2022-04-19 19:08:58 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-BOT_TOKEN = '5033798873:AAEp3qYXjXJEZ8J3T9CTFyEH92JYt9fZxw8'
-PV_USERS = ['marlonviral','vpservice67','Ofcuba']
+BOT_TOKEN = '5253833673:AAEvJbDxfcN3NeYgvQT3UQ4-SLLSPmEW3Wo'
+PV_USERS = ['Jotax2G','vpservice67','Ofcuba']
 
 USERS = {}
 def saveDB():
